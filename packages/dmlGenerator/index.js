2c8c08673514f5f31eedd26762b3ec16bf6af54d
Vadim Orel <v.orel@inbase.com.ua>
2017-01-05 19:28:35 +0200

    - dmlGenerator in separate package(refactoring) where conditions added logicalPredicates added

---
+++ packages/dmlGenerator/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by v.orel on 05.01.2017.
+ * @module
+ */
+const MSSQLBuilder = require ('msSQLBuilder')
+
+exports.buildSelectSql = function (params) {
+  const builder = new MSSQLBuilder(params)
+  return builder.buildSelectQuery()
+}
