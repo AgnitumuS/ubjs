619a36eeae4fb6cf8c383cfed111fb5af82c0324
Vadim Orel <v.orel@inbase.com.ua>
2017-01-05 19:50:32 +0200

    eslint error fixed

--- packages/dmlGenerator/index.js
+++ packages/dmlGenerator/index.js
@@ -2,7 +2,7 @@
  * Created by v.orel on 05.01.2017.
  * @module
  */
-const MSSQLBuilder = require ('msSQLBuilder')
+const MSSQLBuilder = require('msSQLBuilder')
 
 exports.buildSelectSql = function (params) {
   const builder = new MSSQLBuilder(params)
