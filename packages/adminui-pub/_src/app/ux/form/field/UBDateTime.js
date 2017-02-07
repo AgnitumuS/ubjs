d3e42e38a5f6a6f30cf2a79466b65f45b28a86a4
pavelmash <pavel.mash@gmail.com>
2017-02-07 09:54:55 +0200

    Set a minValue for UBDate & UBDateTime adminUI controls to 1901-01-01 to prevent a incorrect serialisation of such dates on server

--- packages/adminui-pub/_src/app/ux/form/field/UBDateTime.js
+++ packages/adminui-pub/_src/app/ux/form/field/UBDateTime.js
@@ -3,21 +3,23 @@ require('../../../../ux/form/DateTimeField')
  * UnityBase ext-based DateTime picker
  */
 Ext.define('UB.ux.form.field.UBDateTime', {
-    extend: 'Ext.ux.form.DateTimeField',
-    alias: 'widget.ubdatetimefield',
-    minWidth: 160,
-    constructor: function(config) {
-        var me = this;
+  extend: 'Ext.ux.form.DateTimeField',
+  alias: 'widget.ubdatetimefield',
+  minWidth: 160,
+  // the minimum date value server handle correctly is 1900-01-01
+  minValue: new Date(1901, 1, 1),
+  constructor: function (config) {
+    var me = this
 
-        config = config || {};
-        if (!config.width){
-            config.width = ((config.fieldLabel) ? config.labelWidth || me.labelWidth || 100 : 0)   + 160;
-        }
-        if (config.fieldLabel){
-            me.minWidth = config.width;
-        }
-        me.maxWidth = config.maxWidth || config.width;
-        me.callParent([config]);
+    config = config || {}
+    if (!config.width) {
+      config.width = ((config.fieldLabel) ? config.labelWidth || me.labelWidth || 100 : 0) + 160
     }
+    if (config.fieldLabel) {
+      me.minWidth = config.width
+    }
+    me.maxWidth = config.maxWidth || config.width
+    me.callParent([config])
+  }
 
-});
+})
