d6751ff958fe3fbc7e9aa4a71c855ae2048a2159
MaxLoboda <max2loboda@gmail.com>
2017-03-08 19:38:47 +0200

    new event registrationStart. It was required for check referral code

--- packages/uba/public/registration.js
+++ packages/uba/public/registration.js
@@ -24,6 +24,14 @@ $(document).ready(function () {
                         cmpSelect.append($('<option>', params));
                     });
                     cmpSelect.prop('disabled', false );
+                    var scodes = data.map(function(r){ return r.code});
+                    Object.keys(incomeParam).forEach(function(key){
+                        if (scodes.indexOf(key) >= 0){
+                            $('#utm_campaign').val(key).change();
+                            $('#utm_source').val(incomeParam[key]);
+                            $("#utm_source_group").removeClass('hidden');
+                        }
+                    })
                 });
         }
     }).done();
@@ -38,8 +46,22 @@ $(document).ready(function () {
         }
     });
 
+    var incomeParam = {};
+    if (location.search) {
+        location.search.substr(1).split("&").forEach(function (pstr) {
+            var vp = pstr.split('=');
+            incomeParam[vp[0]] = decodeURIComponent(vp[1]);
+        });
+        if (incomeParam.email) {
+            $(this).find('#email').val(incomeParam.email);
+        }
+        if (incomeParam.phone) {
+            $(this).find('#phone').val(incomeParam.phone);
+        }
+    }
+
     frm.on('submit', function (e) {
-        if (grecaptcha.getResponse() === ''){
+        if (!window.norecapcha && (grecaptcha.getResponse() === '')){
             e.preventDefault();
             var alertText = "Please, confirm you are not a robot";
             $('#registration-form').find('.messages').html('<div class="alert alert-danger" role="alert"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
@@ -58,7 +80,7 @@ $(document).ready(function () {
                     phone: $(this).find('#phone').val(),
                     utmCampaign: $(this).find('#utm_campaign').val(),
                     utmSource: $(this).find('#utm_source').val(),
-                    recaptca: grecaptcha.getResponse()
+                    recaptca: window.norecapcha ? '': grecaptcha.getResponse()
                 })
             }).done(function(data){
                 console.log(data);
@@ -70,15 +92,20 @@ $(document).ready(function () {
             }).fail(function(reason){
                 console.error(reason);
                 var alertText;
+                var inErr;
                 var abortRe = /^UBAbort: /;
                 if (reason && reason.responseJSON && abortRe.test(reason.responseJSON.errMsg)){
-                    var inErr = reason.responseJSON.errMsg;
+                    inErr = reason.responseJSON.errMsg;
                     inErr = /^UBAbort: <<<(.*)>>>$/.test(inErr) ? /^UBAbort: <<<(.*)>>>$/.exec(inErr)[1] : /^UBAbort: (.*)$/.exec(inErr)[1];
                     if (inErr === 'Duplicate user name (may be in different case)') {
                         alertText = 'Member with such EMail is already registered. If you forgot your password, please go to the "Member Area" and select "Need help?" option'
                     } else {
                         alertText = inErr;
                     }
+                } else if (reason && reason.responseJSON && /<<<(.*)>>>/.test(reason.responseJSON.errMsg)){
+                    inErr = reason.responseJSON.errMsg;
+                    inErr = /<<<(.*)>>>/.exec(inErr)[1];
+                    alertText = inErr? inErr: reason.responseJSON.errMsg;
                 } else {
                     alertText = 'Error during processing your request. Please, try again later of <a href="contacts.html">contact us</a>'
                 }
@@ -86,7 +113,7 @@ $(document).ready(function () {
                     alertText + '</div>';
                 //frm[0].reset();
                 frm.find('.messages').html(alertBox);
-                grecaptcha.reset();
+                if (!window.norecapcha) grecaptcha.reset();
             });
             return false;
         }
