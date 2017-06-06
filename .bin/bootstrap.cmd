d6b84a5b6b56eff08bd78429136f0695a92315e6
pavel.mash <pavel.mash@inbase.com.ua>
2017-06-06 19:17:38 +0300

    new replace-native command in autotest app package.json for installing compiled native modules

--- .bin/bootstrap.cmd
+++ .bin/bootstrap.cmd
@@ -27,4 +27,19 @@ if exist ..\ub-d\packages (
   echo otherwise remove all @ub-d/* models from .\apps\autotest\ubConfig*.json 
 )
 
+if not defined SRC (
+  echo To compile a native modules you need:
+  echo  - checkout a UnityBase server sources 
+  echo  - set environment variable SRC to Source folder location
+  echo  - execute command `npm run build:native`
+  echo
+  echo Compilation use a Delphi@XE2-sp4 Delphi@7 and InnoSetup@5.5.9 - all of them MUST be installed
+  echo
+  echo In case you do not have access to Server Sources or compilers - run a `npm run replace-native` in autotest folder
+  echo this command will install latest compiled packages from registry
+  exit 1
+)
+
+call npm run build:native
+
 
