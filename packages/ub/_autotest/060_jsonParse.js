b2201772f590675022adf7f6285185f3fe22ed04
softpro <forvac@gmail.com>
2017-12-24 17:56:36 +0200

    Windows specific path delimiter was used

--- packages/ub/_autotest/060_jsonParse.js
+++ packages/ub/_autotest/060_jsonParse.js
@@ -1,6 +1,7 @@
 var argv = require('@unitybase/base').argv
 var assert = require('assert')
 var path = require('path')
+var pathDelim = process.platform === 'win32' ? '\\' : '/'
 var fixturesDir = path.join(path.dirname(__filename), 'nodeModules', 'fixtures')
-var obj = argv.safeParseJSONfile(fixturesDir + '\\jsonParser.json')
+var obj = argv.safeParseJSONfile(fixturesDir + pathDelim + 'jsonParser.json')
 assert.equal(obj.path, '\\\\fs\\Share\\')
