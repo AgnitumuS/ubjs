ee4ad9fb59e29022edf21ec6d9c5c38e34ba1939
softpro <forvac@gmail.com>
2018-04-02 16:58:42 +0300

    Folder name corrected 'paralel' -> 'parallel'

--- packages/ub/_autotest/010_nodeModules.js
+++ packages/ub/_autotest/010_nodeModules.js
@@ -6,13 +6,15 @@ const path = require('path')
 
 let tests = fs.readdirSync(path.dirname(__filename) + '/nodeModules/simple')
 tests.forEach(function (test) {
+  console.log('Run', test)
   console.debug('Run', test)
   require('./nodeModules/simple/' + test)
 })
 
-tests = fs.readdirSync(path.dirname(__filename) + '/nodeModules/paralel')
+tests = fs.readdirSync(path.dirname(__filename) + '/nodeModules/parallel').sort()
 tests.forEach(function (test) {
   if (test.substr(0, 4) === 'test') {
+    console.log('Run', test)
     console.debug('Run', test)
     require('./nodeModules/paralel/' + test)
   }
