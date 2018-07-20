fc8c6bed17c8f8b258a088b4a3706e89422d02c2
pavel.mash <7467039+pavelmash@users.noreply.github.com>
2018-07-20 21:17:03 +0300

    entity oiverride test added

--- apps/autotest/models/TST/_autotest/0100_DDLGenerator.js
+++ apps/autotest/models/TST/_autotest/0100_DDLGenerator.js
@@ -37,6 +37,9 @@ function runTest (conn) {
     })
   }, /VALUE_MUST_BE_UNIQUE/, 'Unique constraint in database must fail for tst_dictionaly update from "code10" to "code20"')
 
+  console.log('check ubm_desktop is overrided by TST model')
+  data = conn.Repository('ubm_desktop').attrs(['ID', 'overrided']).limit(1).selectAsObject()
+
     // testcase for TubDataStore.generateDDL() call in case data store is created in one context but called in other
   conn.get('getIDTest')
 }
