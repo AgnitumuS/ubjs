354fe12082ad4c1cf9c0a50aa4f618cac45e382e
pavel.mash <7467039+pavelmash@users.noreply.github.com>
2018-09-05 16:28:18 +0300

    code formatting (no functional changes)

--- packages/adminui-pub/_src/app/view/UBForm.js
+++ packages/adminui-pub/_src/app/view/UBForm.js
@@ -1,58 +1,58 @@
+/* global Ext */
 /*
  * MPV: This is a hack to correctly determinate form fields. TODO - do it via override of Ext.form.Basic?
  */
 Ext.define('UB.view.UBForm', {
-    extend: 'Ext.form.Basic',
+  extend: 'Ext.form.Basic',
 
-    constructor: function(owner, config) {
-        this.callParent([owner, config]);
-        this.getFields();
-    },
+  constructor: function (owner, config) {
+    this.callParent([owner, config])
+    this.getFields()
+  },
 
-
-    getFields: function() {
-        var me = this;
-        if (!me.formFields){
-            var forms, excluded = [], items;
-            me.formFields = new Ext.util.MixedCollection();
-            forms = me.owner.query('form');
-            Ext.each(forms, function(dform){
-                if (dform !== me.owner){
-                    Ext.each( dform.query('[isFormField]'), function(detIiem){
-                      excluded.push(detIiem);
-                    },me);
-                }
-            },me);
-            items = me.owner.query(me.monitor.selector);
-            Ext.each(excluded, function(eitem){
-                var delItem = items.indexOf(eitem);
-                if (delItem >= 0){
-                    items.splice(delItem,1);
-                }
-            },me);
-
-            me.formFields.addAll(items);
+  getFields: function () {
+    let me = this
+    if (!me.formFields) {
+      let excluded = []
+      me.formFields = new Ext.util.MixedCollection()
+      let forms = me.owner.query('form')
+      Ext.each(forms, function (dform) {
+        if (dform !== me.owner) {
+          Ext.each(dform.query('[isFormField]'), function (detIiem) {
+            excluded.push(detIiem)
+          }, me)
         }
-        return me.formFields;
-    },
-
-    onFieldAdd: function(field){
-        var me = this;
-        if (me.owner === field.up('form')){
-            me.callParent(arguments);
-            if (me.formFields){
-                me.formFields.addAll([field]);
-            }
+      }, me)
+      let items = me.owner.query(me.monitor.selector)
+      Ext.each(excluded, function (eitem) {
+        var delItem = items.indexOf(eitem)
+        if (delItem >= 0) {
+          items.splice(delItem, 1)
         }
-    },
+      }, me)
 
-    onFieldRemove: function(field){
-        var me = this;
-        if (me.owner === field.up('form')){
-            me.callParent(arguments);
-            if (me.formFields){
-                me.formFields.push(field);
-            }
-        }
+      me.formFields.addAll(items)
     }
-});
+    return me.formFields
+  },
+
+  onFieldAdd: function (field) {
+    let me = this
+    if (me.owner === field.up('form')) {
+      me.callParent(arguments)
+      if (me.formFields) {
+        me.formFields.addAll([field])
+      }
+    }
+  },
+
+  onFieldRemove: function (field) {
+    let me = this
+    if (me.owner === field.up('form')) {
+      me.callParent(arguments)
+      if (me.formFields) {
+        me.formFields.push(field)
+      }
+    }
+  }
+})
