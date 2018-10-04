ccb3904a2c0db0fba31e69356ea36f3d0f9346a6
pavel.mash <7467039+pavelmash@users.noreply.github.com>
2018-10-04 12:20:29 +0300

    Exporters code formatting (no functional changes)

--- packages/adminui-pub/_src/ux/exporter/excelFormatter/ExcelFormatter.js
+++ packages/adminui-pub/_src/ux/exporter/excelFormatter/ExcelFormatter.js
@@ -3,21 +3,21 @@
  * @class Ext.ux.Exporter.ExcelFormatter
  * @extends Ext.ux.Exporter.Formatter
  */
-Ext.define("Ext.ux.exporter.excelFormatter.ExcelFormatter", {
-    extend: "Ext.ux.exporter.Formatter",
-    uses: [
-        "Ext.ux.exporter.excelFormatter.Cell",
-        "Ext.ux.exporter.excelFormatter.Style",
-        "Ext.ux.exporter.excelFormatter.Worksheet",
-        "Ext.ux.exporter.excelFormatter.Workbook"
-    ],
-    contentType: 'data:application/vnd.ms-excel;base64,',
-    extension: "xls",
+Ext.define('Ext.ux.exporter.excelFormatter.ExcelFormatter', {
+  extend: 'Ext.ux.exporter.Formatter',
+  uses: [
+    'Ext.ux.exporter.excelFormatter.Cell',
+    'Ext.ux.exporter.excelFormatter.Style',
+    'Ext.ux.exporter.excelFormatter.Worksheet',
+    'Ext.ux.exporter.excelFormatter.Workbook'
+  ],
+  contentType: 'data:application/vnd.ms-excel;base64,',
+  extension: 'xls',
 
-    format: function(store, config) {
-      var workbook =  Ext.create('Ext.ux.exporter.excelFormatter.Workbook',config);
-      workbook.addWorksheet(store, config || {});
+  format: function (store, config) {
+    var workbook = Ext.create('Ext.ux.exporter.excelFormatter.Workbook', config)
+    workbook.addWorksheet(store, config || {})
 
-      return workbook.render();
-    }
-});
+    return workbook.render()
+  }
+})
