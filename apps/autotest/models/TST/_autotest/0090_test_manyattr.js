eb9dfa089076326a5a8fed4cda76fe183b8b01df
pavel.mash <pavel.mash@inbase.com.ua>
2018-10-23 19:38:21 +0300

    add autotest for select many attribute linked property (manyValue.caption)

--- apps/autotest/models/TST/_autotest/0090_test_manyattr.js
+++ apps/autotest/models/TST/_autotest/0090_test_manyattr.js
@@ -1,117 +1,114 @@
-﻿/**
+/**
  * @author pavel.mash
  * Date: 17.10.14
- * Test clobTruncate mixin
+ * Test "many" attribute
  */
-var
-    assert = require('assert'),
-    fs = require('fs'),
-    argv = require('@unitybase/base').argv,
-    session, conn;
+const assert = require('assert')
+const cmdLineOpt = require('@unitybase/base').options
+const argv = require('@unitybase/base').argv
+const TEST_NAME = 'Many attribute'
 
-if (argv.findCmdLineSwitch('help') !== -1){
-    console.info([
-        'Test "Many" attribute. tst_maindata, tst_dictionary entities require',
-        'Usage: ',
-            '>UB ' + __fileName + ' ' + argv.establishConnectionFromCmdLineAttributesUsageInfo
-    ].join('\r\n'));
-    return;
-}
+module.exports = function runFTSTest (options) {
+  if (!options) {
+    let opts = cmdLineOpt.describe('', TEST_NAME)
+      .add(argv.establishConnectionFromCmdLineAttributes._cmdLineParams)
+    options = opts.parseVerbose({}, true)
+    if (!options) return
+  }
 
-session = argv.establishConnectionFromCmdLineAttributes();
-conn = session.connection;
+  let session = argv.establishConnectionFromCmdLineAttributes(options)
+  let conn = session.connection
 
-try {
-    console.debug('testManyAttribute');
-    testManyAttribute(conn);
-} finally {
-    session.logout();
+  console.debug('Start ' + TEST_NAME)
+  testManyAttribute(conn)
 }
 
 /**
  *
  * @param {SyncConnection} conn
  */
-function testManyAttribute(conn){
+function testManyAttribute (conn) {
+  let selected
+  selected = conn.run({
+    entity: 'tst_maindata',
+    method: 'select',
+    fieldList: ['ID', 'manyValue']
+  })
+  assert.equal(selected.resultData.rowCount, 16, 'Total row count in "tst_maindata" must be 16')
 
-    var selected = conn.run({
-        entity: 'tst_maindata',
-		method: 'select',
-        fieldList: ['ID', 'manyValue']
-    });
-    assert.equal(selected.resultData.rowCount, 16, 'Total row count in "tst_maindata" must be 16');
+  // console.debug('Проверка корректности агрегированного значения атрибута "manyValue"');
+  selected = conn.run({
+    entity: 'tst_maindata',
+    method: 'select',
+    fieldList: ['ID', 'manyValue'],
+    whereList: {
+      w1: {
+        expression: '[code]',
+        condition: '=',
+        values: { v1: 'Код1' }
+      }
+    }
+  })
+  assert.equal(selected.resultData.data[0][1], '1,2', 'For row with code="Код1" many must contain 2 value: 1 и 2')
 
-    //console.debug('Проверка корректности агрегированного значения атрибута "manyValue"');
-    var selected = conn.run({
-      entity: 'tst_maindata',
-			method: 'select',
-      fieldList: ['ID', 'manyValue'],
-			whereList: {
-				w1: {
-					expression: '[code]',
-					condition: '=',
-					values: {v1: 'Код1'}
-				}
-			}
-    });
-	assert.equal(selected.resultData.data[0][1], '1,2', 'For row with code="Код1" many must contain 2 value: 1 и 2');
+  // console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "IN". Важно одновременно этот же атрибут иметь в fieldList');
+  selected = conn.run({
+    entity: 'tst_maindata',
+    method: 'select',
+    fieldList: ['ID', 'manyValue'],
+    whereList: {
+      w1: {
+        expression: '[manyValue]',
+        condition: 'in',
+        values: { v1: [6] }
+      }
+    }
+  })
+  assert.equal(selected.resultData.rowCount, 3, '"manyValue IN (6)" condition must rerurn 3 row')
 
-    //console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "IN". Важно одновременно этот же атрибут иметь в fieldList');
-    var selected = conn.run({
-        entity: 'tst_maindata',
-		method: 'select',
-        fieldList: ['ID', 'manyValue'],
-		whereList: {
-			w1: {
-				expression: '[manyValue]',
-				condition: 'in',
-				values: {v1: [6]}
-			}
-		}
-    });
-    assert.equal(selected.resultData.rowCount, 3, '"manyValue IN (6)" condition must rerurn 3 row');
+  // console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "NOT IN". Важно одновременно этот же атрибут иметь в fieldList');
+  selected = conn.run({
+    entity: 'tst_maindata',
+    method: 'select',
+    fieldList: ['ID', 'manyValue'],
+    whereList: {
+      w1: {
+        expression: '[manyValue]',
+        condition: 'notIn',
+        values: { v1: [6] }
+      }
+    }
+  })
+  // Должно быть 14 записей
+  assert.equal(selected.resultData.rowCount, 13, '"manyValue NOT IN (6)" condition must rerurn 13 row')
 
-    //console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "NOT IN". Важно одновременно этот же атрибут иметь в fieldList');
-    var selected = conn.run({
-        entity: 'tst_maindata',
-		method: 'select',
-        fieldList: ['ID', 'manyValue'],
-		whereList: {
-			w1: {
-				expression: '[manyValue]',
-				condition: 'notIn',
-				values: {v1: [6]}
-			}
-		}
-    });
-	//Должно быть 14 записей
-    assert.equal(selected.resultData.rowCount, 13, '"manyValue NOT IN (6)" condition must rerurn 13 row');
+  // console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "IS NULL". Важно одновременно этот же атрибут иметь в fieldList');
+  selected = conn.run({
+    entity: 'tst_maindata',
+    method: 'select',
+    fieldList: ['ID', 'manyValue'],
+    whereList: {
+      w1: {
+        expression: '[manyValue]',
+        condition: 'isNull'
+      }
+    }
+  })
+  assert.equal(selected.resultData.rowCount, 0, '"manyValue IS NULL" condition with manyValue in fieldList must return 0 row')
 
-	//console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "IS NULL". Важно одновременно этот же атрибут иметь в fieldList');
-    var selected = conn.run({
-        entity: 'tst_maindata',
-		method: 'select',
-        fieldList: ['ID', 'manyValue'],
-		whereList: {
-			w1: {
-				expression: '[manyValue]',
-				condition: 'isNull'
-			}
-		}
-    });
-    assert.equal(selected.resultData.rowCount, 0, '"manyValue IS NULL" condition with manyValue in fieldList must return 0 row');
-
-	//console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "IS NOT NULL". Важно одновременно этот же атрибут иметь в fieldList');
-    var selected = conn.run({
-        entity: 'tst_maindata',
-		method: 'select',
-        fieldList: ['ID', 'manyValue'],
-		whereList: {
-			w1: {
-				expression: '[manyValue]',
-				condition: 'isNotNull'
-			}
-		}
-    });
-    assert.equal(selected.resultData.rowCount, 16, '"manyValue IS NOT NULL" condition with manyValue in fieldList must return 16 row');
+  // console.debug('Проверка корректности фильтрации по атрибуту "manyValue" условием "IS NOT NULL". Важно одновременно этот же атрибут иметь в fieldList');
+  selected = conn.run({
+    entity: 'tst_maindata',
+    method: 'select',
+    fieldList: ['ID', 'manyValue'],
+    whereList: {
+      w1: {
+        expression: '[manyValue]',
+        condition: 'isNotNull'
+      }
+    }
+  })
+  assert.equal(selected.resultData.rowCount, 16, '"manyValue IS NOT NULL" condition with manyValue in fieldList must return 16 row')
+  selected = conn.Repository('tst_maindata').attrs('ID', 'manyValue.caption').where('code', '=', 'Код1').selectAsObject()
+  assert.equal(selected[0]['manyValue.caption'], 'caption 10,caption 20', 'select manyValue.caption')
 }
--- apps/autotest/models/TST/_autotest/0100_DDLGenerator.js
+++ apps/autotest/models/TST/_autotest/0100_DDLGenerator.js
@@ -2,38 +2,46 @@
  * Date: 25.09.16
  * Test DDL generator
  */
-var
-  assert = require('assert'),
-  fs = require('fs'),
-  argv = require('@unitybase/base').argv,
-  session, conn
+const assert = require('assert')
+const argv = require('@unitybase/base').argv
+const cmdLineOpt = require('@unitybase/base').options
+const TEST_NAME = 'DDL generator'
 
-session = argv.establishConnectionFromCmdLineAttributes()
-conn = session.connection
+module.exports = function runFTSTest (options) {
+  if (!options) {
+    let opts = cmdLineOpt.describe('', TEST_NAME)
+      .add(argv.establishConnectionFromCmdLineAttributes._cmdLineParams)
+    options = opts.parseVerbose({}, true)
+    if (!options) return
+  }
 
-try {
-  console.debug('test DDL generator')
+  let session = argv.establishConnectionFromCmdLineAttributes(options)
+  let conn = session.connection
+
+  console.debug('Start ' + TEST_NAME)
   runTest(conn)
-} finally {
-  session.logout()
 }
 
+/**
+ *
+ * @param {SyncConnection} conn
+ */
 function runTest (conn) {
-  var dictID = conn.lookup('tst_dictionary', 'ID', {expression: 'code', condition: 'equal', values: {code: 'code10'}})
+  let dictID = conn.lookup('tst_dictionary', 'ID', {expression: 'code', condition: 'equal', values: {code: 'code10'}})
   assert.throws(function () {
     conn.run({
-	        entity: 'tst_dictionary',
-	        method: 'delete',
+      entity: 'tst_dictionary',
+      method: 'delete',
       execParams: { ID: dictID }
     })
   }, /ERR_REFERENCE_ERROR/, 'Referential constraint in database must fail for tst_dictionaly with code="code10" deletion')
 
-  var data = conn.Repository('tst_dictionary').attrs(['mi_modifyDate']).where('ID', '=', dictID).selectAsObject()
+  let data = conn.Repository('tst_dictionary').attrs(['mi_modifyDate']).where('ID', '=', dictID).selectAsObject()
   assert.throws(function () {
     conn.run({
-	        entity: 'tst_dictionary',
-	        method: 'update',
-      execParams: { ID: dictID, code: 'code20', mi_modifyDate: data[0].mi_modifyDate}
+      entity: 'tst_dictionary',
+      method: 'update',
+      execParams: { ID: dictID, code: 'code20', mi_modifyDate: data[0].mi_modifyDate }
     })
   }, /VALUE_MUST_BE_UNIQUE/, 'Unique constraint in database must fail for tst_dictionaly update from "code10" to "code20"')
 
