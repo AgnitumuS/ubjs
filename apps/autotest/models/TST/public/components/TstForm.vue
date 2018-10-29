944bd9ede8ac5d47f35e626900cb888f58852db8
pavel.mash <pavel.mash@inbase.com.ua>
2018-10-29 14:53:28 +0200

    advanced vue js form sample with validators

--- apps/autotest/models/TST/public/components/TstForm.vue
+++ apps/autotest/models/TST/public/components/TstForm.vue
@@ -1,21 +1,32 @@
 <template>
-  <el-form :size="formSize">
-    <tst-hello :whom="formSize ? formSize : 'default'"></tst-hello>
-    <el-form-item>
-      <el-input placeholder="Pavel"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-date-picker type="date"></el-date-picker>
-    </el-form-item>
-    <el-form-item>
-      <el-date-picker
-        type="daterange"
-        range-separator="-"
-        start-placeholder="Start date"
-        end-placeholder="End date">
-      </el-date-picker>
-    </el-form-item>
-  </el-form>
+  <div>
+    <el-form :size="formSize"  @submit.native.prevent :model="testModel" :rules="testRules">
+      <el-form-item prop="uName">
+        <el-input placeholder="enter name" v-model="testModel.uName"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker type="date" v-model="testModel.udate"></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker
+          type="daterange"
+          v-model="testModel.dateRange"
+          range-separator="-"
+          start-placeholder="Start date"
+          end-placeholder="End date">
+        </el-date-picker>
+      </el-form-item>
+      <p> Model data: {{JSON.stringify(testModel)}}</p>
+    </el-form>
+    <el-form :size="formSize" :model="otherTest">
+      <tst-hello :whom="helloTo"></tst-hello>
+      <p>Form size: {{formSize}}</p>
+      <span>Other data binding == {{otherTest.a}}</span>
+      <el-form-item>
+        <el-input v-model="otherTest.a"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -29,7 +40,23 @@
         default: ''
       }
     },
-    data: function () { return { }}
+    data: function () { return {
+      helloTo: 'UnityBase vue',
+      testModel: {
+        uName: '',
+        udate: null,
+        dateRange: [null, null]
+      },
+      otherTest: {
+        a: 1, b: 2
+      },
+      testRules: {
+        uName: [
+          { required: true, message: 'Please input user name', trigger: 'blur' },
+          { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' }
+        ]
+      }
+    }}
   }
 </script>
 
