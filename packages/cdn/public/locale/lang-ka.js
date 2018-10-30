10e4536bca8d7d89b99be2e12f0a7ada48c3bd04
Sergey Severyn <sergey.severyn@softengi.com>
2018-10-30 14:22:19 +0500

    a few commonly used public files translated to Georgian

--- packages/cdn/public/locale/lang-ka.js
+++ packages/cdn/public/locale/lang-ka.js
@@ -1,15 +1,15 @@
-/**
+﻿/**
  * UnityBase Common Dictionaries (CDN) model EN localization
  */
 UB.i18nExtend({
-  externalOrganizations_title: "Organizations",
-  physPerson_title: "Phis. person",
-  locality_title: "Locality",
-  contact_title: "Contact",
-  department_title: "Department of external organization",
-  employee_title: "Employee of an external organization",
-  profession_title: "Profession",
-  region_title: "Region",
+  externalOrganizations_title: "ორგანიზაციები",
+  physPerson_title: "ფიზიკური პირი",
+  locality_title: "უბანი",
+  contact_title: "კონტაქტი",
+  department_title: "გარე ორგანიზაციის დეპარტამენტი",
+  employee_title: "გარე ორგანიზაციის თანამშრომელი",
+  profession_title: "პროფესია",
+  region_title: "რეგიონი",
 
   // !!!  Do not change this last item. Put your new items before EOF for SVN auto merge reason.
   EOF: 'EOF'
