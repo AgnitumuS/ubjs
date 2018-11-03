c2a554f3f0267bc17719b4be300fdc9cb67b6209
pavel.mash <pavel.mash@inbase.com.ua>
2018-11-04 00:18:41 +0200

    tst_ditionary form - destroy Vue instance on form destruction

--- apps/autotest/models/TST/public/forms/tst_dictionary-ft-fm.js
+++ apps/autotest/models/TST/public/forms/tst_dictionary-ft-fm.js
@@ -1,24 +1,19 @@
 const Vue = require('vue')
 exports.formCode = {
-  initComponentDone: function () {
-    const me = this
-    me.on('formDataReady', me.onFormDataReady)
-  },
   initUBComponent: function () {
     let me = this
-    me.vueDate = new Vue({
+    let htmlEl = this.query('[forVue]')[0].getEl().dom.firstChild
+    me.vm = new Vue({
       template: `<el-date-picker type="date" v-model="docDate"></el-date-picker>`,
+      el: htmlEl,
       data: function () {
         return {
           docDate: new Date()
         }
       }
     })
-  },
-  onFormDataReady () {
-    const me = this
-    // insert Vue after component for attribute `booleanColumn`
-    let htmlEl = this.query('[attributeName=booleanColumn]')[0].getEl().dom
-    me.vueDate.$mount(htmlEl)
+    me.on('destroy', () => {
+      me.vm.$destroy()
+    })
   }
 }
