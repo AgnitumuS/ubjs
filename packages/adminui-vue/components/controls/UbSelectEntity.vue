46fb93e10f230de508c94d5298886673dd6b0fd4
vladyslav.loza <vladyslav.loza@inbase.com.ua>
2019-01-04 15:12:58 +0200

    tooltip to the left

--- packages/adminui-vue/components/controls/UbSelectEntity.vue
+++ packages/adminui-vue/components/controls/UbSelectEntity.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-tooltip :content="deletedCaption" placement="top" :disabled="!rowIsDeleted">
+    <el-tooltip :content="deletedCaption" placement="left" :disabled="!rowIsDeleted">
       <el-select ref="selector" v-model="resultData"
                  reserve-keyword filterable remote
                  v-loading="loading" :disabled="loading"
