899fc15a22229c04d862a63a105d6980c8b44cec
vladyslav.loza <vladyslav.loza@inbase.com.ua>
2019-02-27 18:21:27 +0200

    done with input story

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,16 +1,16 @@
 import {
-    storiesOf
-} from '@storybook/vue';
+  storiesOf
+} from '@storybook/vue'
 
-import inputStory from './controls/inputStory.vue';
-import selectStory from './controls/selectStory.vue';
+import inputStory from './controls/inputStory.vue'
+import selectStory from './controls/selectStory.vue'
 
 const paddedList = () => {
-    return {
-        template: '<div style="padding: 20px;"><story/></div>'
-    };
-};
+  return {
+    template: `<div style="padding: 20px; font-family: 'Segoe UI',serif"><story/></div>`
+  }
+}
 
 storiesOf('Vue Controls', module).addDecorator(paddedList)
-    .add('Input', () => inputStory)
-    .add('Select', () => selectStory);
+  .add('Input', () => inputStory)
+  .add('Select', () => selectStory)
