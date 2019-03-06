183a62d6a4c94cfd5e255422187f0f83717e56cc
vladyslav.loza <vladyslav.loza@inbase.com.ua>
2019-03-06 12:47:11 +0200

    add description to entity edit component

--- storybook/stories/components/entityEditStory.vue
+++ storybook/stories/components/entityEditStory.vue
@@ -29,9 +29,11 @@
     <h2>Props</h2>
     <ul>
       <li>
-        <span class="input-story__prop">value - Object</span>
+        <span class="input-story__prop required">value - Object *</span>
+        <el-tooltip effect="light" content="Required" placement="right" :open-delay="100">
         <el-input :disabled="true" type="textarea" :rows="6" style="width: 300px"
                   v-model="JSON.stringify(value, null, 2)"></el-input>
+        </el-tooltip>
       </li>
       <li>
         <span class="input-story__prop">instanceID - Number</span>
@@ -67,7 +69,7 @@
       </li>
     </ul>
     <h2>Props for toolbar</h2>
-    <h3>Check on ToolBar component</h3>
+    <h3 @click="goToToolbar" class="entity-edit-story__link">Check ToolBar component</h3>
     <ul>
       <li>
         <span class="input-story__prop">formCode - String</span>
@@ -82,6 +84,15 @@
         <span class="input-story__prop">inputButtons - Array of objects</span>
       </li>
     </ul>
+    <h2>Description</h2>
+    <div style="padding: 0 30px;">
+      <p>This component helps in creating cards on VueJS.</p>
+      <p style="margin-bottom: 0">While it mounted and value doesn't have properties then component loads data by instanceID (or create new entity if instanceID is empty) and set it to value.</p>
+      <p style="font-size: 0.9em; color: #000;margin-top: 0">value is required! you can set it by your own or send empty object like output parameter</p>
+      <p>All logic with saving/removing implemented inside the component. If you want to add additional logic before saving (validation for example) then you can use save prop of component.
+        Function that you send in this property will be executed while one of savings is emitted.
+        It has callback property with parent save implementation, you should call it in case to save entity or write your own saving.</p>
+    </div>
   </div>
 </template>
 
@@ -90,6 +101,7 @@ import ubEntityEdit from '@unitybase/adminui-vue/components/UbEntityEditComponen
 import UbInput from '@unitybase/adminui-vue/components/controls/UbInput.vue'
 import UbSelectEntity from '@unitybase/adminui-vue/components/controls/UbSelectEntity.vue'
 import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
 export default {
   components: {
@@ -111,8 +123,21 @@ export default {
     }
   },
   methods: {
-    inputFn: action('Entered entity'),
+    goToToolbar: linkTo('Vue Components', 'ToolBar'),
+    inputFn: action('Loaded entity'),
     save: action('It\'s save emitted')
   }
 }
 </script>
+
+<style>
+  .entity-edit-story__link {
+    cursor: pointer;
+    color: rgb(47, 103, 159);
+    display: block;
+    width: 250px;
+  }
+  .entity-edit-story__link:hover {
+    color: rgb(43, 65, 117);
+  }
+</style>
