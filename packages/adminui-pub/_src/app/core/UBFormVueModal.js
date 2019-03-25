16dc0dbd5b5575064bf26d8781982b7dd1170d57
dmitriy rudnik <Dmytro.Rudnyk@softengi.com>
2019-03-25 15:30:54 +0200

    feat(adminui-pub): added isModal prop for vue forms

---
+++ packages/adminui-pub/_src/app/core/UBFormVueModal.js
@@ -0,0 +1,54 @@
+const Vue = require('vue')
+
+function mountModal ({ FormComponent, title }) {
+  const div = document.createElement('div')
+  document.body.append(div)
+  const instance = new Vue({
+    components: { FormComponent },
+    data () {
+      return {
+        dialogVisible: false,
+        title
+      }
+    },
+    template: `
+      <el-dialog
+        :title="title"
+        :visible.sync="dialogVisible"
+        width="90%"
+        @closed="$destroy()"
+      >
+        <form-component />
+      </el-dialog>
+    `
+  })
+  instance.$mount(div)
+
+  instance.dialogVisible = true
+}
+
+function mountTab ({ FormComponent, title }) {
+  const tab = $App.viewport.centralPanel.add({
+    title,
+    style: {
+      padding: '1em' // we replace panel inner content below so set paddings here
+    },
+    closable: true
+  })
+  const instance = new Vue(FormComponent)
+  instance.$mount(`#${tab.getId()}-outerCt`) // simplify layouts by replacing Ext Panel inned content
+  tab.on('close', () => {
+    instance.$destroy()
+  })
+  $App.viewport.centralPanel.setActiveTab(tab)
+}
+
+module.exports = (config) => {
+  if (!config.FormComponent) throw new Error('FormComponent is required')
+
+  if (config.isModal) {
+    mountModal(config)
+  } else {
+    mountTab(config)
+  }
+}
