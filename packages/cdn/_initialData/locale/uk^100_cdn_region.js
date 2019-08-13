7205131396e012b3a9c3f10c98aa212c06d19b2c
pavel.mash <pavel.mash@inbase.com.ua>
2019-08-13 10:47:15 +0300

    cdn_region localization fixed (comma as csv separator, fill caption)

--- packages/cdn/_initialData/locale/uk^100_cdn_region.js
+++ packages/cdn/_initialData/locale/uk^100_cdn_region.js
@@ -9,36 +9,38 @@ const fs = require('fs')
  */
 module.exports = function (session) {
   let localizationConfig = {
-    entity: 'cdn_country',
-    keyAttribute: 'intCode',
+    entity: 'cdn_region',
+    keyAttribute: 'code',
     localization: [
       // {keyValue: 'CHECK',  execParams: {name: 'Розрахунковий'}}
     ]
   }
   let rawData = fs.readFileSync(__filename.replace(/\.js$/, '_ukraine.csv'), 'utf8')
-  let rows = CSV.parse(rawData)
+  let rows = CSV.parse(rawData, ',')
   if (!rows.length) {
-    console.error('CSV file with country localisation is empty')
+    console.error('CSV file with region localisation is empty')
     return
   }
-  let intCodeIndex = rows[0].indexOf('intCode')
-  let nameIndex = rows[0].indexOf('name')
-  let fullNameIndex = rows[0].indexOf('fullName')
-  if (intCodeIndex === -1 || nameIndex === -1 || fullNameIndex === -1) {
-    console.error('CSV file must contains a columnus with names incCode, name, fullName')
+  let codeI = rows[0].indexOf('code')
+  let regionTypeI = rows[0].indexOf('regionType')
+  let nameI = rows[0].indexOf('name')
+  let fullNameI = rows[0].indexOf('fullName')
+  if (codeI === -1 || nameI === -1 || regionTypeI === -1 || fullNameI === -1) {
+    console.error('CSV file must contains a columns with names code, regionType, name, fullName')
     return
   }
   for (let r = 1, l = rows.length; r < l; r++) {
-    if (rows[r][intCodeIndex]) {
+    if (rows[r][codeI]) {
       localizationConfig.localization.push({
-        keyValue: rows[r][intCodeIndex],
+        keyValue: rows[r][codeI],
         execParams: {
-          name: rows[r][nameIndex],
-          fullName: rows[r][fullNameIndex]
+          name: rows[r][nameI],
+          caption: rows[r][nameI],
+          fullName: rows[r][fullNameI]
         }
       })
     } else {
-      console.warn(`Empty intCode in row ${r}`)
+      console.warn(`Empty code in row ${r}`)
     }
   }
   loader.localizeEntity(session, localizationConfig, __filename)
