5f6c426249d1d8414caf7a33afbd58f5ac87ce83
pavel.mash <pavel.mash@inbase.com.ua>
2019-11-30 18:37:13 +0200

    mailer & com bridge JSCLASS_FOREGROUND_FINALIZE

--- apps/autotest/models/TST/public/initModel.js
+++ apps/autotest/models/TST/public/initModel.js
@@ -1,2 +1,6 @@
 const _ = require('lodash')
+const localVue = require('vue')
+localVue.component('aaa', {})
+if (typeof window.Vue.options.components.aaa !== 'function') throw new Error('Fake vue component must exists in global Vue')
+if (typeof localVue.options.components.aaa !== 'function') throw new Error('Fake vue component must exists in local Vue')
 require('./src/test-module.js')
