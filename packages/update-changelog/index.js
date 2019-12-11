25a834609a4cab75829188bc3bc40d6b04a25a38
pavel.mash <pavel.mash@inbase.com.ua>
2019-12-11 14:03:07 +0200

    prevent error in case package do not have CHANGELOG.md

--- packages/update-changelog/index.js
+++ packages/update-changelog/index.js
@@ -1,16 +1,19 @@
-const { readFileSync, writeFileSync } = require('fs')
+const fs = require('fs')
+const path = require('path')
 
-const filename = 'CHANGELOG.md'
-const version = process.env.npm_package_version
-const unreleasedLine = '## [Unreleased]'
-const unreleasedSection = `${unreleasedLine}\n\n`
-const fileLines = readFileSync(filename, 'utf-8').split('\n')
-const newFileLines = fileLines.map((line, index) => {
-  if (line === unreleasedLine && fileLines[index + 1] !== '') {
-    const currentDateInFormat = new Date().toISOString().slice(0, 10)
-    const lineToReplace = `${unreleasedSection}## [${version}] - ${currentDateInFormat}`
-    return lineToReplace
-  }
-  return line
-})
-writeFileSync(filename, newFileLines.join('\n'))
+const filename = path.join(process.cwd(), 'CHANGELOG.md')
+if (fs.existsSync(filename)) {
+  const version = process.env.npm_package_version
+  const unreleasedLine = '## [Unreleased]'
+  const unreleasedSection = `${unreleasedLine}\n\n`
+  const fileLines = fs.readFileSync(filename, 'utf-8').split('\n')
+  const newFileLines = fileLines.map((line, index) => {
+    if (line === unreleasedLine && fileLines[index + 1] !== '') {
+      const currentDateInFormat = new Date().toISOString().slice(0, 10)
+      const lineToReplace = `${unreleasedSection}## [${version}] - ${currentDateInFormat}`
+      return lineToReplace
+    }
+    return line
+  })
+  fs.writeFileSync(filename, newFileLines.join('\n'))
+}
