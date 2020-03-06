03020fb06cad5fc907dbd9d0ec4b7928fc0d8002
pavel.mash <pavel.mash@inbase.com.ua>
2020-03-06 19:31:45 +0200

    - use options.clearable: false in eGroup enum filter - fix typos in CHANGELOG.md

--- packages/adminui-vue/components/controls/USelectEnum.vue
+++ packages/adminui-vue/components/controls/USelectEnum.vue
@@ -89,7 +89,7 @@ export default {
     getEnumRequest () {
       return this.$UB.Repository(this.enumEntity)
         .attrs('eGroup', this.valueAttribute, this.displayAttribute, 'sortOrder')
-        .where('eGroup', '=', this.eGroup, null, true)
+        .where('eGroup', '=', this.eGroup, { clearable: false })
         .orderBy('sortOrder')
     }
   }
