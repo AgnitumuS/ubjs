29bf525e448bd66696e55932f49c7c613b61c30c
dmitriy rudnik <Dmytro.Rudnyk@softengi.com>
2020-04-07 01:43:10 +0300

    test(adminui-vue) lookups

--- packages/adminui-vue/_tests/utils/lookups.js
+++ packages/adminui-vue/_tests/utils/lookups.js
@@ -1,50 +1,32 @@
 require('../helpers/install-xhr')
-const { entities } = require('../../utils/lookups')
+const lookups = require('../../utils/lookups')
 const assert = require('assert')
 
-const lookups = {
-  uba_user: {
-    132321: {
-      value: 'admin',
-      id: 132321
-    }
-  },
-
-  tst_dictionary: {
-    code1: {
-      value: 'code 1',
-      id: 312
-    }
-  }
-}
-
-const reletaions = {
-  tst_dictionary: {
-    312: 'code1'
-  }
-}
-
-// lookups.uba_user.132321
-// lookups.uba_user.1
-// lookups.tst_dictionary.code1
-// search by id
-
-const enums = {
-  DFX_USER_ACTIONS: {
-    INSERT: {
-      value: 'Insert',
-      id: 213123213
-    }
-  }
-}
-
-// enums.DFX_USER_ACTIONS.INSERT
-// enums.DFX_USER_ACTIONS213.INSERT
+// TODO: write tests after @pavel.mash create mocks for UB.ClientRepository
 
 describe('Lookups', () => {
-  describe('#Get lookup value', () => {
+  describe('#init', () => {
+    it('Shall create empty entities subscriptions', () => {
+      assert()
+    })
+  })
+  describe('#subscribe', () => {
+    it('Shall load data on first subscription', () => {
+      assert()
+    })
+
+    it('Shall not load data on repeat subscription', () => {})
+
+    it('Shall load data on repeat subscription if adds attrs', () => {})
+  })
+  describe('#unsubscribe', () => {
     it('', () => {
-      assert(entities.tst_dictionary[10] === 'code 10')
+      assert()
+    })
+  })
+  describe('#get', () => {
+    it('', () => {
+      assert()
     })
   })
 })
