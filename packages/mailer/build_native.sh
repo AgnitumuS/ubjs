96028e8a5f6e0b7973a3a18f5b02d76f081bd5ad
pavel.mash <pavel.mash@inbase.com.ua>
2020-07-12 14:57:24 +0300

    build mailer wo lazbuild (on linuxfor a while)

---
+++ packages/mailer/build_native.sh
@@ -0,0 +1,40 @@
+# -Scgi <------>- Support operators like C; Enable LABEL and GOTO(default for -MDelphi; Inlining
+# -Cg PIC code >- for Linux library only (slowed code for program)
+# -Ci <><------>- IO checking
+# -O2 <><------>- optimization level
+# -g -gl -gw2 -Xg- Generate debug information; Use line info unit (show more info with backtraces); DWARFv2 debug info; debug info in separate file
+# -k'-rpath=$ORIGIN' - link to a library in the same folder as program
+# -veiq -vw-n-h - verbose(errors, info, message numbers) no warnings, no notes, no hints
+# -B <-><------>- build all
+# -Se10 <------>- halts after 10 error.
+# to switch to x64MM -dFPC_SYNCMEM should be removed and -dFPC_X64MM -dFPCMM_SERVER added
+
+mkdir -p ./.dcu/fpc-{linux,win64} ./bin/x86_64
+
+UB_SRC=../../../ub-server
+echo $UB_SRC/libs/Synopse/SyNode
+
+fpc.sh -MDelphi -Sci -Ci -O2 -g -gl -gw2 -Xg -k-Lbin/fpc-linux -Tlinux -Px86_64 \
+  -veiq -vw-n-h- \
+  -Fi.dcu/fpc-linux -Fi$UB_SRC/libs/Synopse -Fi$UB_SRC/libs/Synopse/SQLite3 -Fi$UB_SRC/libs/Synopse/SyNode -Fi$UB_SRC/libs/synapse40/source/lib \
+  -Fl$UB_SRC/bin/fpc-linux \
+  -Fu$UB_SRC/libs/Synopse -Fu$UB_SRC/libs/Synopse/SQLite3 -Fu$UB_SRC/libs/Synopse/SyNode -Fu$UB_SRC/libs/synapse40/source/lib -Fu$LAZARUS_PATH/components/lazutils/lib/x86_64-linux -Fu. \
+  -FU.dcu/fpc-linux -FEbin/x86_64 -obin/x86_64/libubmail.so \
+  -dFPC_SYNCMEM \
+  -B -Se1 ./src/ubmail.dpr
+
+fpc.sh -MDelphi -Sci -Ci -O2 -g -gl -gw2 -Xg -k-Lbin/fpc-linux -Twin64 -Px86_64 \
+  -veiq -vw-n-h- \
+  -Fi.dcu/fpc-win64 -Fi$UB_SRC/libs/Synopse -Fi$UB_SRC/libs/Synopse/SQLite3 -Fi$UB_SRC/libs/Synopse/SyNode -Fi$UB_SRC/libs/synapse40/source/lib \
+  -Fu$UB_SRC/libs/Synopse -Fu$UB_SRC/libs/Synopse/SQLite3 -Fu$UB_SRC/libs/Synopse/SyNode -Fu$UB_SRC/libs/synapse40/source/lib -Fu$LAZARUS_PATH/components/lazutils/lib/x86_64-win64 -Fu. \
+  -FU.dcu/fpc-win64 -FEbin/x86_64 -obin/x86_64/ubmail.dll \
+  -dFPC_SYNCMEM \
+  -B -Se1 ./src/ubmail.dpr
+
+#/home/pavelmash/fpc/fpc/bin/x86_64-linux/fpc.sh
+#-Tlinux -Px86_64 -MDelphi -Scghi -Cg -O2 -veibq -vw-n-h-
+#-Fi../../../../ub-server/libs/Synopse/SyNode  -Fi../../../../ub-server/libs/Synopse -Fi../../../../ub-server/libs/synapse40/source/lib
+# -Fi../lib/x86_64-linux -Fl../../../../ub-server/bin/fpc-linux -Fu../../../../ub-server/libs/Synopse/SyNode
+# -Fu../../../../ub-server/libs/Synopse -Fu../../../../ub-server/libs/Synopse/SQLite3 -Fu../../../../ub-server/libs/synapse40/source/lib
+# -Fu../../../../../fpc/lazarus/components/lazutils/lib/x86_64-linux -Fu../../../../../fpc/lazarus/packager/units/x86_64-linux
+# -Fu. -FU../lib/x86_64-linux -FE../bin/x86_64 -o../bin/x86_64
