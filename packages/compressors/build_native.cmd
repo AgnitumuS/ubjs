721a3feafc68f42551a5e300259bb902e012c99e
pavel.mash <pavel.mash@inbase.com.ua>
2020-07-21 09:51:34 +0300

    windows build for mailer and compressors

--- packages/compressors/build_native.cmd
+++ packages/compressors/build_native.cmd
@@ -1,39 +1,36 @@
-# -Scgi <------>- Support operators like C; Enable LABEL and GOTO(default for -MDelphi; Inlining
-# -Cg PIC code >- for Linux library only (slowed code for program)
-# -Ci <><------>- IO checking
-# -O2 <><------>- optimization level
-# -g -gl -gw2 -Xg- Generate debug information; Use line info unit (show more info with backtraces); DWARFv2 debug info; debug info in separate file
-# -k'-rpath=$ORIGIN' - link to a library in the same folder as program
-# -veiq -vw-n-h - verbose(errors, info, message numbers) no warnings, no notes, no hints
-# -B <-><------>- build all
-# -Se10 <------>- halts after 10 error.
-# to switch to x64MM -dFPC_SYNCMEM should be removed and -dFPC_X64MM -dFPCMM_SERVER added
+rem -Scgi <------>- Support operators like C; Enable LABEL and GOTO(default for -MDelphi; Inlining
+rem -Cg PIC code >- for Linux library only (slowed code for program)
+rem -Ci <><------>- IO checking
+rem -O2 <><------>- optimization level
+rem -g -gl -gw2 -Xg- Generate debug information; Use line info unit (show more info with backtraces); DWARFv2 debug info; debug info in separate file
+rem -k'-rpath=$ORIGIN' - link to a library in the same folder as program
+rem -veiq -vw-n-h - verbose(errors, info, message numbers) no warnings, no notes, no hints
+rem -B <-><------>- build all
+rem -Se10 <------>- halts after 10 error.
+rem to switch to x64MM -dFPC_SYNCMEM should be removed and -dFPC_X64MM -dFPCMM_SERVER added
 
-md .dcu/fpc-linux
-md .dcu/fpc-win64
-md bin/x86_64
+SET TARGET=win64
+SET ARCH=x86_64
+
+md .\bin\%ARCH%
 
 if [%UB_SRC%]==[] (
   SET UB_SRC=../../../ub-server
 )
+if [%LAZ_DIR%]==[] (
+  SET LAZ_DIR==C:/lazarus
+)
 
-fpc.exe -MDelphi -Sci -Ci -O2 -g -gl -gw2 -Xg -k-Lbin/fpc-linux -Tlinux -Px86_64 ^
+SET TARGET=win64
+md .\.dcu\fpc-%TARGET%
+%LAZ_DIR%/fpc/3.2.0/bin/%ARCH%-%TARGET%/fpc.exe -MDelphi -Sci -Ci -O2 -k-Lbin/fpc-linux -Twin64 -Px86_64 ^
   -veiq -vw-n-h- ^
-  -Fi.dcu/fpc-linux -Fi$UB_SRC/libs/Synopse -Fi$UB_SRC/libs/Synopse/SQLite3 -Fi$UB_SRC/libs/Synopse/SyNode -Fi$UB_SRC/libs/synapse40/source/lib ^
-  -Fl$UB_SRC/bin/fpc-linux ^
-  -Fu$UB_SRC/libs/Synopse -Fu$UB_SRC/libs/Synopse/SQLite3 -Fu$UB_SRC/libs/Synopse/SyNode -Fu$UB_SRC/libs/synapse40/source/lib -Fu$UB_SRC/libs/lazutils -Fu. ^
-  -FU.dcu/fpc-linux -FEbin/x86_64 -obin/x86_64/libubmail.so ^
+  -Fi.dcu/fpc-win64 -Fi%UB_SRC%/libs/Synopse -Fi%UB_SRC%/libs/Synopse/SQLite3 -Fi%UB_SRC%/libs/Synopse/SyNode -Fi%UB_SRC%/libs/synapse40/source/lib ^
+  -Fu%UB_SRC%/libs/Synopse -Fu%UB_SRC%/libs/Synopse/SQLite3 -Fu%UB_SRC%/libs/Synopse/SyNode -Fu%UB_SRC%/libs/synapse40/source/lib ^
+  -Fu%LAZ_DIR%/lcl/units/%ARCH%-%TARGET% -Fu%LAZ_DIR%/components/lazutils/lib/%ARCH%-%TARGET% -Fu%LAZ_DIR%/lcl ^
+  -FU.dcu/fpc-win64 -FEbin/x86_64 -obin/x86_64/ubcompressors.dll ^
   -dFPC_SYNCMEM ^
-  -B -Se1 ./src/ubmail.dpr
-@if errorlevel 1 goto err
-
-fpc.exe -MDelphi -Sci -Ci -O2 -k-Lbin/fpc-linux -Twin64 -Px86_64 ^
-  -veiq -vw-n-h- ^
-  -Fi.dcu/fpc-win64 -Fi$UB_SRC/libs/Synopse -Fi$UB_SRC/libs/Synopse/SQLite3 -Fi$UB_SRC/libs/Synopse/SyNode -Fi$UB_SRC/libs/synapse40/source/lib ^
-  -Fu$UB_SRC/libs/Synopse -Fu$UB_SRC/libs/Synopse/SQLite3 -Fu$UB_SRC/libs/Synopse/SyNode -Fu$UB_SRC/libs/synapse40/source/lib -Fu$UB_SRC/libs/lazutils -Fu. ^
-  -FU.dcu/fpc-win64 -FEbin/x86_64 -obin/x86_64/ubmail.dll ^
-  -dFPC_SYNCMEM ^
-  -B -Se1 ./src/ubmail.dpr
+  -B -Se1 ./src/ubcompressors.dpr
 @if errorlevel 1 goto err
 
 goto :eof
