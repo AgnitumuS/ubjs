e418e6f1250c22780c14e5b9110a2aa6aec90459
volodymyr <vovamazur3@gmail.com>
2020-09-16 14:55:31 +0300

    fix migration

--- packages/ubm/_migrations/5.4.14_5.4.15.js
+++ packages/ubm/_migrations/5.4.14_5.4.15.js
@@ -7,27 +7,23 @@ copyFromAdmToAclTable('ubm_navshortcut')
 
 function copyFromAdmToAclTable (entityName) {
   const admEntityName = `${entityName}_adm`
-  const aclEntityNAme = `${entityName}_acl`
+  const aclEntityName = `${entityName}_acl`
 
   console.info(`\tStart moving access info for '${entityName}' from '${admEntityName}' to '${aclEntityNAme}' `)
 
-  const admEntries = connection.Repository(admEntityName)
+  connection.Repository(admEntityName)
     .attrs('ID', 'instanceID', 'admSubjID')
     .selectAsObject()
-
-  const requests = admEntries.reduce((requests, { instanceID, admSubjID }) => {
-    return requests.concat({
-      entity: aclEntityNAme,
-      method: 'insert',
-      execParams: {
-        instanceID,
-        valueID: admSubjID,
-        subjID: admSubjID
-      }
+    .forEach(({ instanceID, admSubjID }) => {
+      connection.insert({
+        entity: aclEntityName,
+        execParams: {
+          instanceID,
+          valueID: admSubjID,
+          subjID: admSubjID
+        }
+      })
     })
-  }, [])
-
-  connection.runTans(requests)
 
   console.info(`\tEnd moving access info for '${entityName}' from '${admEntityName}' to '${aclEntityNAme}' `)
 }
