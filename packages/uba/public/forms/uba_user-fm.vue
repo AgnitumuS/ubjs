ce0ec40bc30b224da13718455b474ba614ed3dcc
dmytro.rudnyk <dmytro.rudnyk@inbase.com.ua>
2020-10-12 13:02:36 +0300

    WIP: refact uba_user as vue

---
+++ packages/uba/public/forms/uba_user-fm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form-root />
+</template>
+
+<script>
+const { Form } = require('@unitybase/adminui-vue')
+
+module.exports.mount = cfg => {
+  // uba_userrole
+  // uba_usergroup
+  // uba_usercertificate
+  Form(cfg)
+    .processing({
+      masterFieldList: [
+        'firstName',
+        'name',
+        'lastName',
+        'fullName',
+        'gender',
+        'email',
+        'phone',
+        'description',
+        'disabled',
+        'isPending',
+        'trustedIP',
+        'uData',
+        'avatar'
+      ]
+    })
+    .validation()
+    .mount()
+}
+
+module.exports.default = {
+  name: 'UserForm',
+  components: {
+    FormRoot: require('./uba_user/FormRoot.vue').default
+  }
+}
+</script>
---
+++ packages/uba/public/forms/uba_user/FormRoot.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="u-form-layout">
+    <u-toolbar>
+      <u-button
+        slot="append"
+        appearance="inverse"
+      >
+        ActionChangePasswordID
+      </u-button>
+    </u-toolbar>
+    <u-form-container>
+      <u-auto-field attribute-name="firstName" />
+      <u-auto-field attribute-name="name" />
+      <u-auto-field attribute-name="lastName" />
+      <u-auto-field attribute-name="fullName" />
+      <u-auto-field attribute-name="gender" />
+      <u-auto-field attribute-name="email" />
+      <u-auto-field attribute-name="phone" />
+      <hr>
+      <u-auto-field attribute-name="description" />
+      <u-auto-field attribute-name="disabled" />
+      <u-auto-field attribute-name="isPending" />
+      <u-auto-field attribute-name="trustedIP" />
+      <u-auto-field attribute-name="uData" />
+      <u-auto-field attribute-name="avatar" />
+    </u-form-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserFormRoot'
+}
+</script>
