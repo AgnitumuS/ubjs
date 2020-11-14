32357e65021c42c391baf5cad20f1e00d1783975
pavel.mash <pavel.mash@inbase.com.ua>
2020-11-14 18:48:05 +0200

    setDocumentPerfTest implemented (require UB 5.18.19 because uses  crypto.fillRandomSync)

--- apps/autotest/appAutotest.js
+++ apps/autotest/appAutotest.js
@@ -1,8 +1,9 @@
 const UB = require('@unitybase/ub')
 const App = UB.App
 
-// BLOB store performance test endpoint. For TEST environments only
-const {getDocumentPerfTestEp} = require('@unitybase/blob-stores/storesPerfTest.js')
+// BLOB store performance test endpoints. For TEST environments only
+const {getDocumentPerfTestEp, setDocumentPerfTestEp} = require('@unitybase/blob-stores/storesPerfTest.js')
 App.registerEndpoint('getDocumentPerfTest', getDocumentPerfTestEp, false)
+App.registerEndpoint('setDocumentPerfTest', setDocumentPerfTestEp, false)
 
 UB.start()
--- packages/blob-stores/storesPerfTest.js
+++ packages/blob-stores/storesPerfTest.js
@@ -13,12 +13,23 @@
  * 2) For a real-life results ensure reverseProxy.kind="nginx" in config
  *
  * 2) Consider we interest in measurement of BLOB store configured for `tst_maindata.fileStoreSimple` attribute
- *   In console:
- *   ensure endpoint works as expected:
+ *   In console
+ *
+ *   Ensure endpoint works as expected:
  *   curl 'http://app.url/getDocumentPerfTest?entity=tst_maindata&attribute=fileStoreSimple'
  *
- *   run a load test
- *   wrk 'http://app.url/getDocumentPerfTest?entity=tst_maindata&attribute=fileStoreSimple'
+ *   Warming up:
+ *   wrk -t 8 -c 100 'http://app.url/getDocumentPerfTest?entity=tst_maindata&attribute=fileStoreSimple'
+ *
+ *   Measure:
+ *   wrk -t 8 -c 100 'http://app.url/getDocumentPerfTest?entity=tst_maindata&attribute=fileStoreSimple'
+ *
+ *   Write performance test:
+ *   wrk -t 8 -c 100 'http://app.url/setDocumentPerfTest?entity=tst_maindata&attribute=fileStoreSimple'
+ *
+ *   Remove test files:
+ *   cd /path/to/store
+ *   find -L . -name *.test -type f -exec rm -rf {} +
  *
  */
 
@@ -26,20 +37,21 @@ const UB = require('@unitybase/ub')
 const Session = UB.Session
 const blobStores = require('./blobStores')
 const queryString = require('querystring')
+const crypto = require('crypto')
 
 module.exports = {
   getDocumentPerfTestEp,
   setDocumentPerfTestEp
 }
 
-const MAX_ROWS = 11
+const MAX_ROWS = 10000
 const idsCache = {
   //entityName: [1, 2, 3] // last MAX_ROWS IDs
 }
 /**
  * Obtains a random (one of last MAX_ROWS (10_000) row) document from specified entity and attribute and send it to response.
  *
- *  GET /testGetDocument?entity=myEntity&attribute=documentAttr
+ *  GET /getDocumentPerfTest?entity=myEntity&attribute=documentAttr
  *
  * @param {THTTPRequest} req
  * @param {THTTPResponse} resp
@@ -57,7 +69,7 @@ function getDocumentPerfTestEp(req, resp) {
   }
 
   const bsReq = {
-    ID: idsCache[entity][Math.round(Math.random()*(idsCache[entity].length-1))],
+    ID: idsCache[entity][randomInt(idsCache[entity].length)],
     entity,
     attribute
   }
@@ -66,15 +78,55 @@ function getDocumentPerfTestEp(req, resp) {
 }
 
 /**
- * Write a file of random ( 1000 - 500_000 bytes) size value to the BLOB store for specified entity and attribute
- * Warning!!
+ * Write a file of a random size to the BLOB store for specified entity and attribute.
  *
- *  GET /testGetDocument?entity=myEntity&attribute=documentAttr
+ * File content is a random string ( 1000 - 50_000 bytes) repeated a random (1-10) times.
+ * This allows testing a file system deduplication and compression (if enabled for store file system)
+ *
+ * **Warning!!** Files are written directly to the BLOB file system and not inserted to the entity.
+ * Dont forget to remove such files after tests.
+ *
+ *      cd /path/to/store
+ *      find -L . -name *.test -type f -exec rm -rf {} +
+ *
+ *  POST /setDocumentPerfTest?entity=myEntity&attribute=documentAttr
  *
  * @param {THTTPRequest} req
  * @param {THTTPResponse} resp
  * @private
  */
 function setDocumentPerfTestEp(req, resp) {
-  return resp.notImplemented('not implemented yet')
+  const {entity, attribute} = queryString.parse(req.parameters)
+  if (!App.domainInfo.has(entity)) {
+    return resp.badRequest('unknown entity')
+  }
+  const attr = App.domainInfo.get(entity).getAttribute(attribute)
+  const chunkLen = randomInt(49000) + 1000
+  const chunkBuf = crypto.randomBytes(chunkLen)
+  const chunkCnt = randomInt(10) + 1
+  const parts = []
+  for (let i = 0; i < chunkCnt; i++) {
+    parts.push(chunkBuf)
+  }
+  const data = Buffer.concat(parts,chunkLen * chunkCnt)
+  const ID = App.dbConnections.DEFAULT.genID(undefined)
+  const dirtyContent = App.blobStores.putContent({
+    entity: entity,
+    attribute: attribute,
+    ID,
+    fileName: ID + '.test'
+  }, data)
+
+  const docContent = blobStores.doCommit(attr, ID, dirtyContent)
+
+  resp.statusCode = 200
+  resp.writeEnd(docContent)
+}
+
+/**
+ * Returns random in [0..maxVal-1]
+ * @return number
+ */
+function randomInt(maxVal) {
+  return Math.round(Math.random()*(maxVal-1))
 }
