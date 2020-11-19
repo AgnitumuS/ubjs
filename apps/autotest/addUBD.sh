87d9574748a0b62e420e0756c9b62339c31fd193
pavel.mash <pavel.mash@inbase.com.ua>
2020-11-19 19:06:13 +0200

    pdfsign test added to autotest app

--- apps/autotest/addUBD.sh
+++ apps/autotest/addUBD.sh
@@ -7,3 +7,9 @@ fi
 if [ ! -d "./node_modules/@ub-d/iit-crypto" ]; then
   ln -s "$(pwd)/../../../ub-d/packages/iit-crypto" "./node_modules/@ub-d/"
 fi
+
+mkdir -p './node_modules/@ub-e'
+
+if [ ! -d "./node_modules/@ub-e/pdfsign" ]; then
+  ln -s "$(pwd)/../../../ub-e/packages/pdfsign" "./node_modules/@ub-e/"
+fi
--- apps/autotest/appAutotest.js
+++ apps/autotest/appAutotest.js
@@ -2,8 +2,23 @@ const UB = require('@unitybase/ub')
 const App = UB.App
 
 // BLOB store performance test endpoints. For TEST environments only
-const {getDocumentPerfTestEp, setDocumentPerfTestEp} = require('@unitybase/blob-stores/storesPerfTest.js')
+const { getDocumentPerfTestEp, setDocumentPerfTestEp } = require('@unitybase/blob-stores/storesPerfTest.js')
 App.registerEndpoint('getDocumentPerfTest', getDocumentPerfTestEp, false)
 App.registerEndpoint('setDocumentPerfTest', setDocumentPerfTestEp, false)
+
+App.registerEndpoint('upload', (req, resp) => { console.log(req.headers) }, false)
+
+App.registerEndpoint('pdfsign', testPdfSignerSpeed, false)
+
+/**
+ *
+ * @param {THTTPRequest} req
+ * @param {THTTPResponse} resp
+ */
+function testPdfSignerSpeed (req, resp) {
+  const pdfsignTest = require('@ub-e/pdfsign/_autotest/test_TubSigner.js')
+  pdfsignTest()
+  resp.statusCode = 200
+}
 
 UB.start()
