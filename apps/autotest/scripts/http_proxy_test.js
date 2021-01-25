d155fd74c06054850f3fd2fed4e723fbd5d7d8a2
pavel.mash <pavel.mash@inbase.com.ua>
2021-01-25 18:48:53 +0200

    http_proxy demo script - added test for request reuse

--- apps/autotest/scripts/http_proxy_test.js
+++ apps/autotest/scripts/http_proxy_test.js
@@ -1,12 +1,26 @@
 const http = require('http')
-let request = http.request({
+let request, resp
+const NUM_REQ = 100
+
+console.time('req per request')
+for(let i=0; i < NUM_REQ; i++) {
+  request = http.request({
     //alternative to host/port/path is
     URL: 'https://unitybase.info/quote',
     method: 'GET',
     sendTimeout: 30000, receiveTimeout: 30000,
     keepAlive: true,
     compressionEnable: true
- })
+  })
 
-let resp = request.end()
-console.log(resp.statusCode, resp.read())
+  resp = request.end()
+  console.log(resp.statusCode, resp.read())
+}
+console.timeEnd('req per request')
+
+console.time('reuse req')
+for (let i=0; i < NUM_REQ; i++) {
+  resp = request.end()
+  console.log(resp.statusCode, resp.read())
+}
+console.timeEnd('reuse req')
