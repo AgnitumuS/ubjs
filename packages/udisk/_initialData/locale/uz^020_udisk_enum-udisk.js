06823529b4ef72aa840706e620f56204e538a502
MerlKori <merlkori2015@gmail.com>
2021-08-18 10:41:24 +0300

    traslations have been supplemented

---
+++ packages/udisk/_initialData/locale/uz^020_udisk_enum-udisk.js
@@ -0,0 +1,21 @@
+﻿/**
+ * @author pavel.mash
+ * Enumeration localization to Russian for UDISK model
+ * Used by cmd\initialize command
+ * @param {cmd.argv.serverSession} session
+ */
+module.exports = function (session) {
+  const loader = require('@unitybase/base').dataLoader
+
+  let localizationConfig = {
+    entity: 'ubm_enum',
+    keyAttribute: 'code',
+    localization: [
+      { keyValue: 'owner', execParams: { name: 'Egasi' } },
+      { keyValue: 'read', execParams: { name: 'O`qish' } },
+      { keyValue: 'write', execParams: { name: 'O`zgartirish' } },
+      { keyValue: 'delegate', execParams: { name: 'Huquqlarni sozlash' } }
+    ]
+  }
+  loader.localizeEntity(session, localizationConfig, __filename)
+}
