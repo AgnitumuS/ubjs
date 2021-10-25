1050923ea33437ee3d4dd439aca2242474073845
dmytro.tarabanov <dmytro.tarabanov@inbase.com.ua>
2021-10-25 10:25:20 +0300

    add validator

--- packages/adminui-vue/components/controls/mixins/selection/props.js
+++ packages/adminui-vue/components/controls/mixins/selection/props.js
@@ -1,6 +1,23 @@
 module.exports = {
   props: {
-    selectedRows: { type: Array, default: () => [] },
+    selectedRows: {
+      type: Array,
+      default: () => [],
+      validator (arr) {
+        if (arr.length === 0) return true
+        const flag = arr.some(i => {
+          const type = typeof i
+          return type !== 'number' || type !== 'string'
+        })
+        if (flag === true) {
+          console.error(
+            ' %c Item on selectedRows array must be number or string',
+            'color: #bada55'
+          )
+        }
+        return !flag
+      }
+    },
     selectionField: { type: String, default: 'ID' },
     multiple: { type: Boolean, default: false }
   }
