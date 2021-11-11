6acafbde28edc74f27de307296c6674376a5a3b0
Dmitriy <zp00v0518@gmail.com>
2021-11-11 15:40:40 +0200

    removed Mutationobserver, added arrow-collapse on bottom, fixув bug which I knew

--- packages/adminui-vue/components/UFieldSet.vue
+++ packages/adminui-vue/components/UFieldSet.vue
@@ -2,8 +2,7 @@
   <div
     class="u-field-set"
     :class="{
-      'u-field-set--collapse': !isExpanded,
-      'u-field-set--card-mode': cardMode
+      'u-field-set--collapse': !isExpanded
     }"
   >
     <div
@@ -11,12 +10,18 @@
       :class="['u-field-set__header--' + legendPosition.toLowerCase()]"
       @click="doExpand"
     >
+      <!-- @slot content will be added instead of title. To cancel collapsing on click on content add `@click.stop` to the content  -->
       <slot name="label">
         <u-button
           appearance="inverse"
           size="small"
           :color="color"
-          :icon="isExpanded ? icons[0] : icons[1]"
+          :icon="
+            iconPosition === 'left' ? (isExpanded ? icons[0] : icons[1]) : ''
+          "
+          :right-icon="
+            iconPosition === 'right' ? (isExpanded ? icons[0] : icons[1]) : ''
+          "
         >
           {{ $ut(legend) }}
         </u-button>
@@ -25,17 +30,33 @@
     <div
       ref="body"
       class="u-field-set__body"
+      :style="style"
+      @transitionend="handlerTransitionend"
     >
-      <slot :name="legend" />
+      <!-- @slot main content. Default slot  -->
+      <slot />
+      <div class="u-field-set__footer">
+        <hr class="u-field-set__footer--line">
+        <u-button
+          appearance="inverse"
+          icon="u-icon-arrow-up"
+          class="u-field-set__footer--icon"
+          @click="doExpand"
+        >
+          <!-- <i class="u-button__icon u-icon-arrow-up" /> -->
+        </u-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'UFieldSet',
   props: {
+    /**
+     * title text
+     */
     legend: {
       type: String,
       default: ''
@@ -45,60 +66,71 @@ export default {
       default: 'left',
       validator: val => ['left', 'center', 'right'].includes(val)
     },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: val => ['left', 'right'].includes(val)
+    },
     color: {
       type: String,
       default: 'primary'
-    },
-    cardMode: {
-      type: Boolean,
-      default: false
     },
     icons: {
       type: Array,
       default: () => ['u-icon-circle-minus', 'u-icon-circle-plus'],
       validator: val => val.length === 2
     },
-    dynamicContent: {
+    initialExpanded: {
       type: Boolean,
       default: true
     }
   },
   data () {
     return {
-      isExpanded: true,
-      observer: null
+      isExpanded: this.initialExpanded,
+      style: {
+        maxHeight: 'unset',
+        paddingTop: '',
+        paddingBottom: '',
+        opacity: ''
+      }
+    }
+  },
+  created () {
+    if (!this.isExpanded) {
+      this.style.maxHeight = '0px'
+      this.style.paddingTop = '0px'
+      this.style.paddingBottom = '0px'
+      this.style.opacity = '0'
     }
   },
   mounted () {
     this.$nextTick(() => {
-      this.setStyles()
-      if (this.dynamicContent) this.setObserver()
+      this.handlerTransitionend()
     })
   },
-  beforeDestroy () {
-    if (this.observer) this.observer.disconnect()
-  },
   methods: {
+    handlerTransitionend () {
+      const { style } = this
+      if (this.isExpanded && style.maxHeight !== '0px') {
+        style.maxHeight = 'unset'
+      }
+    },
     doExpand () {
       this.isExpanded = !this.isExpanded
       this.setStyles()
     },
     setStyles () {
       const { body } = this.$refs
-      body.style.maxHeight = this.isExpanded ? body.scrollHeight + 'px' : '0px'
-      body.style.paddingTop = this.isExpanded ? '' : '0px'
-      body.style.paddingBottom = this.isExpanded ? '' : '0px'
-      body.style.opacity = this.isExpanded ? '1' : '0'
-    },
-    setObserver () {
-      const { body } = this.$refs
-      if (!body) return
-      const config = {
-        childList: true,
-        subtree: true
-      }
-      this.observer = new MutationObserver(() => this.setStyles())
-      this.observer.observe(body, config)
+      body.style.maxHeight = body.scrollHeight + 'px'
+      setTimeout(() => {
+        this.style.maxHeight = this.isExpanded
+          ? body.style.scrollHeight + 'px'
+          : '0px'
+        this.style.paddingTop = this.isExpanded ? '' : '0px'
+        this.style.paddingBottom = this.isExpanded ? '' : '0px'
+        this.style.opacity = this.isExpanded ? '1' : '0'
+      }, 0)
     }
   }
 }
@@ -117,21 +149,6 @@ export default {
   margin-bottom: var(--shift);
 }
 
-.u-field-set--card-mode {
-  margin-left: var(--shift);
-  margin-right: var(--shift);
-  box-shadow: 0px 4px 12px 0 rgb(0 0 0 / 15%);
-}
-.u-field-set--card-mode .u-field-set__body {
-  padding-left: var(--shift);
-  padding-right: var(--shift);
-}
-.u-field-set--collapse .u-field-set__header {
-  background-color: hsl(var(--hs-primary), var(--l-background-active));
-}
-.u-field-set--collapse .u-field-set__body {
-  border: none;
-}
 .u-field-set__header {
   background-color: hsl(var(--hs-primary), var(--l-background-default));
   position: relative;
@@ -155,7 +172,24 @@ export default {
   overflow: hidden;
   padding: var(--shift) 0;
   box-sizing: content-box;
-  border-top: var(--borderStyle);
-  border-bottom: var(--borderStyle);
+}
+.u-field-set__footer {
+  position: relative;
+  margin-top: 16px;
+}
+.u-field-set__footer--line {
+  border-color: hsl(var(--hs-border), var(--l-layout-border-light));
+  border-top: none;
+}
+.u-field-set__footer--icon {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 0 8px;
+  border: none;
+  color: hsl(var(--hs-text), var(--l-text-label));
+  background-color: white;
+  cursor: pointer;
 }
 </style>
