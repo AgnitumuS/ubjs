e778c36d9f22d3f3cda096dcb8c8c8df2dc85885
Dmitriy <zp00v0518@gmail.com>
2021-11-25 17:32:04 +0200

    refactoring: it's props includes in mixin

--- packages/adminui-vue/components/UTableEntity/components/UTableEntityRoot.vue
+++ packages/adminui-vue/components/UTableEntity/components/UTableEntityRoot.vue
@@ -565,22 +565,6 @@ export default {
     showDeleteMultipleBtn: {
       type: Boolean,
       default: false
-    },
-    /**
-     * @argument {array<object>} addedCache an array that includes the objects (rows) that will be add to the the selection
-     * Hook that is called before selecting an item. If the hook returns a false value, the selection will be canceled.
-     */
-    beforeAddSelection: {
-      type: Function,
-      default: () => true
-    },
-    /**
-     * @argument {array<object>} removedCache an array that includes the objects (rows) that will be remove from the the selection
-     * Hook that is called before deselecting. If the hook returns a false value, the deselection will be canceled
-     */
-    beforeRemoveSelection: {
-      type: Function,
-      default: () => true
     }
   },
 
