69adb2aa93cfcee48658c390b68afd70373ddf44
Obydonov Oleh <oleh.obydonov@inbase.com.ua>
2022-09-21 11:07:10 +0300

    LDOC-2547 refactoring

--- packages/update-changelog/index.js
+++ packages/update-changelog/index.js
@@ -1,36 +1,44 @@
 const fs = require('fs')
 const path = require('path')
+const normalizeVersion = require('./normalize-version')
 
+updateChangelog(process.env.npm_package_version)
 renameFolderUnreleased(process.env.npm_package_version)
 
-const filename = path.join(process.cwd(), 'CHANGELOG.md')
-if (!fs.existsSync(filename)) process.exit(0)
+/**
+ * Update [Unreleased] in CHANGELOG.md to current package version and add a new Unreleased section at the top
+ *
+ * @param {string} version
+ */
+function updateChangelog (version) {
+  const filename = path.join(process.cwd(), 'CHANGELOG.md')
+  if (!fs.existsSync(filename)) return
 
-const changedSectionRegex = /^## \[Unreleased\]\r?\n(.*\r?\n)+## \[[\d.]+\].*/gm
-const verifyEmptySectionsRegex = /###\s.*\r?\n[\t\s]*\r?\n/g
-const fileContent = fs.readFileSync(filename, 'utf-8')
-const matched = fileContent.match(changedSectionRegex)
-if (matched === null) process.exit(0)
+  const changedSectionRegex = /^## \[Unreleased\]\r?\n(.*\r?\n)+## \[[\d.]+\].*/gm
+  const verifyEmptySectionsRegex = /###\s.*\r?\n[\t\s]*\r?\n/g
+  const fileContent = fs.readFileSync(filename, 'utf-8')
+  const matched = fileContent.match(changedSectionRegex)
+  if (matched === null) return
 
-const unreleasedSubstring = matched[0]
-const unreleasedLine = '## [Unreleased]'
-const typesOfChanges = [
-  '### Added',
-  '### Changed',
-  '### Deprecated',
-  '### Removed',
-  '### Fixed'
-]
-const unreleasedSection = unreleasedLine.concat('\n', typesOfChanges.join('\n\n'), '\n\n')
-const version = process.env.npm_package_version
-const currentDateInFormat = new Date().toISOString().slice(0, 10)
-const newUnreleasedSection = `${unreleasedSection}## [${version}] - ${currentDateInFormat}`
-const updatedUnreleasedSection = unreleasedSubstring
-  .replace(verifyEmptySectionsRegex, '')
-  .replace(unreleasedLine, newUnreleasedSection)
-const newFileContent = fileContent.replace(unreleasedSubstring, updatedUnreleasedSection)
+  const unreleasedSubstring = matched[0]
+  const unreleasedLine = '## [Unreleased]'
+  const typesOfChanges = [
+    '### Added',
+    '### Changed',
+    '### Deprecated',
+    '### Removed',
+    '### Fixed'
+  ]
+  const unreleasedSection = unreleasedLine.concat('\n', typesOfChanges.join('\n\n'), '\n\n')
+  const currentDateInFormat = new Date().toISOString().slice(0, 10)
+  const newUnreleasedSection = `${unreleasedSection}## [${version}] - ${currentDateInFormat}`
+  const updatedUnreleasedSection = unreleasedSubstring
+    .replace(verifyEmptySectionsRegex, '')
+    .replace(unreleasedLine, newUnreleasedSection)
+  const newFileContent = fileContent.replace(unreleasedSubstring, updatedUnreleasedSection)
 
-fs.writeFileSync(filename, newFileContent)
+  fs.writeFileSync(filename, newFileContent)
+}
 
 /**
  * Rename folder _migrate/unreleased to _migrate/[normalized version]
@@ -38,7 +46,6 @@ fs.writeFileSync(filename, newFileContent)
  * @param {string} version
  */
 function renameFolderUnreleased (version) {
-  const normalizeVersion = require('./normalize-version')
   const folderName = path.join(process.cwd(), '_migrate', 'unreleased')
   const newFolder = path.join(process.cwd(), '_migrate', normalizeVersion(version))
 
--- packages/update-changelog/normalize-version.js
+++ packages/update-changelog/normalize-version.js
@@ -1,17 +1,21 @@
-// Normalize of the string to match X.YY.ZZ version pattern
-
-const versionPattern = (x, i) => {
-  let r = x
-  if (i > 0) {
-    const leadingDigits = x.replace(/[^\d].*/, '')
-    r = `${leadingDigits.length < 2 ? ('00' + leadingDigits).slice(-2) : leadingDigits}${x.substring(leadingDigits.length)}`
+/**
+ * Normalize of the string to match X.YY.ZZ version pattern
+ *
+ * @param {string} version - version in semver format
+ * @returns {string} - normalized version
+ */
+module.exports = function normalizeVersion (version) {
+  const versionPattern = (x, i) => {
+    let r = x
+    if (i > 0) {
+      const leadingDigits = x.replace(/\D.*/, '')
+      r = `${leadingDigits.length < 2 ? ('00' + leadingDigits).slice(-2) : leadingDigits}${x.substring(leadingDigits.length)}`
+    }
+    return r
   }
-  return r
-}
 
-const normalizeVersion = (v) => {
-  let r = v
-  const vv = v.split('.')
+  let r = version
+  const vv = version.split('.')
 
   // version contain three part (major.minor.patch)
   if (vv.length >= 3) {
@@ -19,5 +23,3 @@ const normalizeVersion = (v) => {
   }
   return r
 }
-
-module.exports = normalizeVersion
